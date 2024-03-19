b5f2048421cb04a73af56af81375594c1703fd46
Supriyahv9 <109582223+Supriyahv9@users.noreply.github.com>
2024-03-19 16:13:36 +0530

    Add files via upload

--- ListenerImplementation.java
+++ ListenerImplementation.java
@@ -1,34 +1,48 @@
 package CommonUtils;
 
 import org.testng.ITestContext;
+
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class ListenerImplementation implements ITestListener {
 
+	ExtentReports report;
+	
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
 		//System.out.println("testscript execution is started");
-		Reporter.log("Testscript execution is Started",true);
+		String methodname = result.getMethod().getMethodName();
+		Reporter.log(methodname+"Testscript execution is Started",true);
 	}
 
 	public void onTestSuccess(ITestResult result) {
 		// TODO Auto-generated method stub
 	//System.out.println("testscript is passed");	
-		Reporter.log("Testscript execution is Pass",true);
+		//Capture the method name
+		String methodname = result.getMethod().getMethodName();
+		Reporter.log(methodname+"Testscript execution is Pass",true);
 	}
 
 	public void onTestFailure(ITestResult result) {
 		// TODO Auto-generated method stub
 		//System.out.println("testscript is failed");
-		Reporter.log("Testscript execution is Fail",true);
+		//Capture the method name
+	String methodname = result.getMethod().getMethodName();
+	Reporter.log(methodname+"Testscript execution is Fail",true);
 	}
 
 	public void onTestSkipped(ITestResult result) {
 		// TODO Auto-generated method stub
 		//System.out.println("testscript is skipped");
-		Reporter.log("Testscript execution is Skip",true);
+		//Capture the method name
+	String methodname = result.getMethod().getMethodName();
+	Reporter.log(methodname+"Testscript execution is Skip",true);
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
@@ -43,12 +57,25 @@ public class ListenerImplementation implements ITestListener {
 
 	public void onStart(ITestContext context) {
 		// TODO Auto-generated method stub
+	
+		//To configure extent reports
+	ExtentSparkReporter	esr = new ExtentSparkReporter("./Extentreport/report.html");
+	esr.config().setReportName("Vtigercrm");	
+	esr.config().setTheme(Theme.DARK);
+	esr.config().setDocumentTitle("Vtiger");
 		
+	//To generate extent reports
+	report = new ExtentReports();
+	report.attachReporter(esr);
+	report.setSystemInfo("Browser", "Chrome");
+	report.setSystemInfo("Chrome", "122");
+	report.setSystemInfo("OS", "Windows");
+	report.setSystemInfo("Author", "supriya");
 	}
 
 	public void onFinish(ITestContext context) {
 		// TODO Auto-generated method stub
-		
+		report.flush();
 	}
 
 	
--- OrganizationsTest.java
+++ OrganizationsTest.java
@@ -2,53 +2,55 @@ package Vtigercrm;
 
 import java.io.IOException;
 
-import org.apache.poi.EncryptedDocumentException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import CommonUtils.BaseClass;
 import CommonUtils.ExcelUtil;
 import CommonUtils.JavaUtil;
+import CommonUtils.ListenerImplementation;
+import CommonUtils.PropertyFileUtil;
 import CommonUtils.WebDriverUtil;
-
+@Listeners(ListenerImplementation.class)
 public class OrganizationsTest extends BaseClass {
-	
+
+	PropertyFileUtil putil = new PropertyFileUtil();
 	WebDriverUtil wutil = new WebDriverUtil();
-	JavaUtil jutil = new JavaUtil();
 	ExcelUtil eutil = new ExcelUtil();
-	
+	JavaUtil jutil = new JavaUtil();
 	
 	@Test
-	public void CreateOrganizationsTest() throws EncryptedDocumentException, IOException {
+	public void OrganizationTest() throws IOException, InterruptedException {
 		
-		//To read data from Excel File
-	String ORGNAME = eutil.getDataFromExcelFile("Organizations", 0, 1);
-	String GROUP = eutil.getDataFromExcelFile("Organizations", 1, 1);
-		
-		//Click on Organizations
-		driver.findElement(By.xpath("(//a[text()='Organizations'])[1]")).click();
-		
-		//Click on Create Organization..(+)
-		driver.findElement(By.cssSelector("img[alt='Create Organization...']")).click();
-		
-		//Enter Organization name
-		driver.findElement(By.name("accountname")).sendKeys(ORGNAME+jutil.getRandomNumber());
-		
-		//Click on group radio button
+	//To read data from Excel File
+	String ORGNAME = eutil.getDataFromExcel("Organizations", 0, 1);
+	String GROUP = eutil.getDataFromExcel("Organizations", 1, 1);
+	
+	//Click on Organization
+	driver.findElement(By.xpath("(//a[text()='Organizations'])[1]")).click();
+	
+	//Click on Create Organization..(+)
+	driver.findElement(By.cssSelector("img[alt='Create Organization...']")).click();
+	
+	//Enter Organization Name
+	driver.findElement(By.name("accountname")).sendKeys(ORGNAME+jutil.getRandomNumber());
+	
+	//In AssignedTo Click on Group
 	driver.findElement(By.xpath("(//input[@name='assigntype'])[2]")).click();
-		
-		//Select support group in the dropdown
-		WebElement dropdown = driver.findElement(By.name("assigned_group_id"));
-		
-		wutil.handledropdown(dropdown, GROUP);
-		
-		//Click on Save button
-		driver.findElement(By.xpath("(//input[@name='button'])[1]")).click();
-		
-		
-		
-		
-	}
-
+	
+	//In the dropdown select Support group
+	WebElement dropdown = driver.findElement(By.name("assigned_group_id"));
+	wutil.handledropdown(dropdown, GROUP);
+	
+	//Click on Save Button
+	driver.findElement(By.xpath("(//input[@name='button'])[1]")).click();
+	
+	
+	
+	
+}
 }
